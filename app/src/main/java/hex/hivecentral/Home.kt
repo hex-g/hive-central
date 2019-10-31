7e52e6534396d6e5df97749561b883fae7a0d42e
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-10-30 22:34:08 -0300

    Everything2

--- app/src/main/java/hex/hivecentral/Home.kt
+++ app/src/main/java/hex/hivecentral/Home.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class Home : AppCompatActivity() {
     var lastTime = 0L
+    private var adapter: NotesAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,24 @@ class Home : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
-        findViewById<TextView>(R.id.home_textView_welcomeText).text =
-            getString(R.string.home_textView_welcomeText, (application as Application).currentUser)
+        val baseUrl = (application as Application).API_URL
+        val urls = ArrayList<String>()
+
+        // build urls
+        urls.add("/note")
+        urls.add("/note2")
+        urls.add("/note3")
+        urls.add("/note4")
+
+        val notes = ArrayList<NotesAdapter.Note>()
+
+        val queue = Volley.newRequestQueue(this)
+        urls.forEach { path -> {}}
+
+        val stringRequest = StringRequest(baseUrl + "/note", Response.Listener<String> { response -> notes.add(NotesAdapter.Note) })
+
+        adapter = NotesAdapter(this, notes)
+        findViewById<RecyclerView>(R.id.home_recyclerView_notes).adapter = adapter
     }
 
     override fun onBackPressed() {
@@ -27,7 +49,8 @@ class Home : AppCompatActivity() {
         if (currentTime - lastTime <= 2000L) {
             finishAffinity()
         } else {
-            Toast.makeText(this, getString(R.string.home_toast_backpressed), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.home_toast_backpressed), Toast.LENGTH_SHORT)
+                .show()
             lastTime = currentTime
         }
     }
---
+++ app/src/main/java/hex/hivecentral/NotesAdapter.kt
@@ -0,0 +1,38 @@
+package hex.hivecentral
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.io.Serializable
+
+class NotesAdapter(private val context: Context, private val notes: MutableList<Note>) :
+    RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val createdView = LayoutInflater.from(context).inflate(R.layout.item_note, parent, false)
+        return NoteViewHolder(createdView)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.fill(note)
+    }
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val title: TextView = itemView.findViewById(R.id.itemNota_textView_title)
+        private val briefing: TextView = itemView.findViewById(R.id.itemNota_textView_briefing)
+
+        fun fill(note: Note) {
+            title.text = note.title
+            briefing.text = note.briefing
+        }
+    }
+
+    inner class Note(val title: String, val briefing: String) : Serializable
+}
